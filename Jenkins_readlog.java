e499fdb4b93b6503f5ba84a120bab23484a09b77
Tulika Kumari <tulika.kumari@nagarro.com>
2022-01-11 16:48:27 +0530

    jenkins output log

---
+++ Jenkins_readlog.java
@@ -0,0 +1,15 @@
+
+public class Jenkins_readlog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Job is completed");
+		System.out.println("Job is not completed");
+		System.out.println("error");
+		
+		
+		
+	}
+
+}
